f3e044b561a8a9dae11e89e29df39bb89e15f97f
Alex Mutuc <alexanderkmutuc@gmail.com>
2021-02-13 20:50:53 -0800

    1a: Beginning Student Language Practice Problems

--- week1/expressionspt1.rkt
+++ week1/expressionspt1.rkt
@@ -1,5 +1,21 @@
 #lang htdp/bsl
 
+(require 2htdp/image)
+; !racket %
 (sqrt (+ (sqr 3)  (sqr 4)))
 ; 5
+
+
+(circle 10 "solid" "red")
+
+; Declaring constants
+(define WIDTH 400)
+(define HEIGHT 600)
+
+
+(define (bulb c)
+  (circle 40 "solid" c))
+
+(bulb "purple")
+
 
